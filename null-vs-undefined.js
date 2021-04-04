54c88c50cf1f79df44d3342ef16f964046da4320
Jahidul Islam <ziopu05@gmail.com>
2021-04-05 00:44:03 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2) {
+    console.log(num2);
+} add(10);
+
+const premik = { name: "smart dude", phone: 00000000 }
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 54, 42];
+console.log(ages[10]);
