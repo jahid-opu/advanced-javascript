8647d5ebbefb4862c72c853789a3350bc04749fe
Jahidul Islam <ziopu05@gmail.com>
2021-04-05 00:21:15 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// NaN
+// Trythy:
+// '0' ' ';
+let name = false;
+if (name) {
+console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
